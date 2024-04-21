5689b34247eeb498e7b966811b0beffb6f71c6f5
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-04-21 12:18:52 -0400

    save to build

--- scripts/compile.ts
+++ scripts/compile.ts
@@ -4,18 +4,32 @@ import { Cell } from "ton-core";
 import { compileFunc } from "@ton-community/func-js";
 
 async function compileScript() {
+  console.log(
+    "Compile scrupt is running, let's find some FunC code to compile..."
+  );
+
   const compileResult = await compileFunc({
     targets: ["./contracts/main.fc"],
     sources: (x) => fs.readFileSync(x).toString("utf-8"),
   });
-
-  Cell.fromBoc(Buffer.from(compileResult.codeBoc, "base64"))[0]
-    .toBoc()
-    .toString("hex");
-
   if (compileResult.status === "error") {
+    console.log(`Compilation error: ${compileResult.message}`);
     process.exit(1);
   }
+
+  console.log("Compilation was successful");
+
+  const hexArtifiact = "build/main.compiled.json";
+  fs.writeFileSync(
+    hexArtifiact,
+    JSON.stringify({
+      hex: Cell.fromBoc(Buffer.from(compileResult.codeBoc, "base64"))[0]
+        .toBoc()
+        .toString("hex"),
+    })
+  );
+
+  console.log("Compiled code was saved to " + hexArtifiact);
 }
 
 compileScript();
